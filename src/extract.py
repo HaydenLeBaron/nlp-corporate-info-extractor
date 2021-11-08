5b80ac899d8edade8dfc9eca8f7d7a09a38b63ef
Hayden LeBaron <hlebaron98@gmail.com>
2021-11-07 18:40:31 -0700

    Outline extract step

--- src/extract.py
+++ src/extract.py
@@ -3,6 +3,7 @@ Author: Hayden LeBaron
 GitHub: HaydenTheBaron
 Date: Nov 7, 2021
 '''
+# TODO: make robust enough so that it will never crash
 
 import sys
 import pandas as pd
@@ -12,7 +13,34 @@ OUTPUT_DIR_PATH = '../output/'
 
 def extract(doc:str) -> Template:
     """Extracts corporate acquisition info from doc into a Template."""
-    pass
+
+    '''Read document into buffer'''
+    doc_buf = ''
+    with open(doc, 'r') as file:
+        doc_buf = file.read()
+
+    '''Generate candidates via semantic role labeling'''
+    # TODO: From buffer, generate pandas dataframe that effectively maps template fields to candidates and metadata about those candidates (using SRL)
+    '''>
+    # TODO: draw structure of data frame
+    acquired
+    acqbus
+    acqloc
+    dlramt
+    purchaser
+    seller
+    status
+    ...>'''
+    #TODO: implement step
+    '''>Output SR labeled JSON to output files: one file per doc....>'''
+    #TODO: implement
+
+    '''Apply heuristics to find best candidate(s) (if any) for each field'''
+    #TODO
+
+    '''Construct and return template'''
+    return Template(text=doc.split('/')[-1])
+
 
 
 def main():
@@ -30,20 +58,20 @@ def main():
     doc_series = pd.read_table(doclist_file_path, header=None).transpose().iloc[0]
 
     '''Perform information extraction into Template objects'''
-    template_list = []
-    for doc in doc_series:
-        extract(doc)
-
-    #TODO
+    # TODO: initialize template_list to an empty list
     template_list = [Template(text='ShouldbeAllEmpty'),
                      Template(text='123',
-                                acquired=['Acquired 1', 'Acquired 2'],
-                                acqbus=['Acqbus1', 'Acqbus2'],
-                                acqloc=['Acqloc'],
-                                dlramt=['$1'],
-                                purchaser=['Purchaser'],
-                                seller=['Seller'],
-                                status=['Current Status'])]
+                              acquired=['Acquired 1', 'Acquired 2'],
+                              acqbus=['Acqbus1', 'Acqbus2'],
+                              acqloc=['Acqloc'],
+                              dlramt=['$1'],
+                              purchaser=['Purchaser'],
+                              seller=['Seller'],
+                              status=['Current Status'])]
+    for doc in doc_series:
+        template_list.append(extract(doc))
+
+    
 
     '''Write to output file'''
     output_file_path = OUTPUT_DIR_PATH + doclist_file_path.split('/')[-1] + '.templates' 
