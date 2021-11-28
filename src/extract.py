d748e2daaf178147d2ee5c5bd62d884b1827e538
Hayden LeBaron <hlebaron98@gmail.com>
2021-11-28 15:59:19 -0700

    Add srl and perform many experiments. For two fields, filtering with ARG0 increased F-SCORE by 0.1 (not worth the time)

--- src/extract.py
+++ src/extract.py
@@ -34,24 +34,22 @@ def extract(doc_path:str,
     """
 
     '''Run semantic role labler'''
-    '''TODO: uncommentme when we actually use this
     srl_json_str = json.dumps(srl_predictor.label_batch(text_data))
     srl_df = pd.json_normalize(json.loads(srl_json_str), record_path=['verbs'])['description'].tolist()
-    '''
 
-    #if is_verbose : print('SEMANTIC ROLE DATAFRAME:\n{}'.format(srl_df))
+    if is_verbose : print('SEMANTIC ROLE DATAFRAME:\n{}'.format(srl_df))
 
     '''
-    Extract Acqloc:
-           1. Accumulate all "ARGM-LOC" spans of text into a list
+    Extract AGENT:
+           1. Accumulate all "ARG0" spans of text into a list
            2. Map over ARGM-LOC entites, running a NER on each elt. Return a list of places.
            3. Rank options (sort list by rank) (TODO: implement this)
            4. If the list is empty, output '---'. Else choose the highest ranked option.
     '''
 
-    '''1. Accumulate all "ARGM-LOC" entites into a list'''
+    '''1. Accumulate all "ARG0" entites into a list'''
 
-    '''2. Map over ARGM-LOC spans, running a NER on each elt. Return a list of places.'''
+    '''2. Map over ARG0 spans, running a NER on each elt. Return a list of places.'''
     '''> spacy NER labels # TODO: encapsulate these programmatically in a class or something.
        > CARDINAL, DATE, EVENT, FAC, GPE, LANGUAGE, LAW, LOC, MONEY, NORP,
        > ORDINAL, ORG, PERCENT, PERSON, PRODUCT, QUANTITY, TIME, WORK_OF_ART
@@ -60,13 +58,19 @@ def extract(doc_path:str,
 
 
     # Do something like this for some other fields SRL+NER
-    '''
-    argmloc_spans = []
+    arg0_spans = []
     for elt in srl_df:
-        argmloc_spans.append(re.findall('\[ARGM-LOC.*?\]', elt))
-    if is_verbose : print('===ARGMLOC_SPANS:===\n{}'.format(argmloc_spans))
+        arg0_spans.append(re.findall('\[ARG0:.*?\]', elt)) 
 
-    loc_ents = []
+    # Remove empty lists, unnest lists (extract 0th element), and extract 'DATA' from '[ARG0: DATA]'
+    arg0_spans = list(map(lambda s : s[6:-1].strip(),
+                          list(map(lambda ll : ll[0],
+                                   list(filter(lambda l : len(l) != 0, arg0_spans))))))
+
+    if is_verbose : print('===ARG0_SPANS:===\n{}'.format(arg0_spans))
+
+    '''
+    arg0_ents = []
     for span in argmloc_spans: # TODO: use list comprehension
         labeled_span = spacy_model(' '.join(span))
         print('LABELED_SPAN:', labeled_span)
@@ -90,9 +94,15 @@ def extract(doc_path:str,
     for s_dict in text_data:
         labeled_sentences.append(spacy_model(s_dict['sentence']))
 
+    labeled_arg0spans = []
+    for span in arg0_spans:
+        labeled_arg0spans.append(spacy_model(span))
+
     loc_ents = []
     dlramt_ents = []
     org_ents = []
+    '''
+    TODO: uncommentme
     for labeled_span in labeled_sentences:
         for ent in labeled_span.ents:
             print('(ent.text={},ent.label_={})'.format(ent.text, ent.label_))
@@ -102,6 +112,17 @@ def extract(doc_path:str,
                 dlramt_ents.append(ent.text)
             elif ent.label_ == 'ORG':
                 org_ents.append(ent.text)
+    '''
+    for labeled_span in labeled_arg0spans:
+        for ent in labeled_span.ents:
+            print('(ent.text={},ent.label_={})'.format(ent.text, ent.label_))
+            if ent.label_ == 'GPE': #Geo-Political Entity
+                loc_ents.append(ent.text)
+            elif ent.label_ == 'QUANTITY' or ent.label_ == 'MONEY':
+                dlramt_ents.append(ent.text)
+            elif ent.label_ == 'ORG':
+                org_ents.append(ent.text)
+
 
 
         '''
@@ -204,6 +225,71 @@ SELLER          0.00 (0/156)	   0.00 (0/0)         0.00
 STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
 --------        --------------     --------------     ----
 TOTAL           0.28 (469/1693)	   0.19 (469/2449)    0.23
+
+PERFORMANCE FOR USING : x = list(set(arg0_spans)) for purchaser, seller, acquired
+                RECALL             PRECISION          F-SCORE
+ACQUIRED        0.13 (55/418)	   0.02 (55/2639)     0.04
+ACQBUS          0.00 (0/153)	   0.00 (0/0)         0.00
+ACQLOC          0.37 (49/134)	   0.13 (49/367)      0.20
+DLRAMT          0.00 (0/164)	   0.00 (0/0)         0.00
+PURCHASER       0.47 (177/373)	   0.07 (177/2639)    0.12
+SELLER          0.44 (68/156)	   0.03 (68/2639)     0.05
+STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
+--------        --------------     --------------     ----
+TOTAL           0.21 (349/1693)	   0.04 (349/8284)    0.07
+
+PERFORMANCE FOR USING : x = list(set(arg1_spans)) for purchaser, seller, acquired
+                    RECALL             PRECISION          F-SCORE
+ACQUIRED        0.09 (39/418)	   0.01 (39/5235)     0.01
+ACQBUS          0.00 (0/153)	   0.00 (0/0)         0.00
+ACQLOC          0.37 (49/134)	   0.13 (49/367)      0.20
+DLRAMT          0.00 (0/164)	   0.00 (0/0)         0.00
+PURCHASER       0.04 (15/373)	   0.00 (15/5235)     0.01
+SELLER          0.01 (2/156)	   0.00 (2/5235)      0.00
+STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
+--------        --------------     --------------     ----
+TOTAL           0.06 (105/1693)	   0.01 (105/16072)   0.01
+
+PERFORMANCE FOR USING: property = list(set(property_ents)) WHERE property_ents are chosen from ARG0 candidates (instead of all sentences)
+                    RECALL             PRECISION          F-SCORE
+ACQUIRED        0.16 (66/418)	   0.08 (66/808)      0.11
+ACQBUS          0.00 (0/153)	   0.00 (0/0)         0.00
+ACQLOC          0.07 (9/134)	   0.07 (9/131)       0.07
+DLRAMT          0.00 (0/164)	   0.00 (0/0)         0.00
+PURCHASER       0.47 (174/373)	   0.22 (174/808)     0.29
+SELLER          0.44 (68/156)	   0.08 (68/808)      0.14
+STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
+--------        --------------     --------------     ----
+TOTAL           0.19 (317/1693)	   0.12 (317/2555)    0.15
+
+PERFORMANCE FOR USING: property = n_most_common(BEST_WEIGHT, list(property_ents)) WHERE property_ents are chosen from ARG0 candidates (instead of all sentences)
+                    RECALL             PRECISION          F-SCORE
+ACQUIRED        0.16 (65/418)	   0.09 (65/752)      0.11 # WORSE than best
+ACQBUS          0.00 (0/153)	   0.00 (0/564)       0.00 # WORSE than best
+ACQLOC          0.07 (9/134)	   0.07 (9/131)       0.07 # WORSE than best
+DLRAMT          0.00 (0/164)	   0.00 (0/0)         0.00
+PURCHASER       0.40 (150/373)	   0.27 (150/564)     0.32 # BEST so far (0.1 better)
+SELLER          0.43 (67/156)	   0.10 (67/686)      0.16 # BEST SO FAR (0.1 better)
+STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
+--------        --------------     --------------     ----
+TOTAL           0.17 (291/1693)	   0.11 (291/2697)    0.13
+
+
+    PERFORMANCE FOR USING: x = n_most_common(BEST_WEIGHT, x_ents)
+                RECALL             PRECISION          F-SCORE
+ACQUIRED        0.58 (243/418)	   0.19 (243/1309)    0.28
+ACQBUS          0.00 (0/153)	   0.00 (0/0)         0.00
+ACQLOC          0.37 (49/134)	   0.13 (49/367)      0.20
+DLRAMT          0.06 (10/164)	   0.07 (10/151)      0.06
+PURCHASER       0.47 (177/373)	   0.23 (177/773)     0.31
+SELLER          0.60 (93/156)	   0.09 (93/1086)     0.15
+STATUS          0.00 (0/295)	   0.00 (0/0)         0.00
+--------        --------------     --------------     ----
+TOTAL           0.34 (572/1693)	   0.16 (572/3686)    0.21
+
+
+
+
     '''
     n_most_common = lambda n, l : list(map(lambda tuple : tuple[0], Counter(l).most_common(n)))
 
@@ -216,23 +302,26 @@ TOTAL           0.28 (469/1693)	   0.19 (469/2449)    0.23
 
     purchaser = n_most_common(2, org_ents)
     #purchaser = list(set(org_ents))
+    #purchaser = list(set(arg0_spans))
 
 
-    #seller = n_most_common(3, org_ents)
-    seller = [] # Best strategy so far is to leave empty (for f-score, but not recal)
+    acqbus = n_most_common(2, org_ents)
+
+
+    seller = n_most_common(3, org_ents)
+    #seller = [] # Best strategy so far is to leave empty (for f-score, but not recal)
+    #seller = list(set(arg0_spans))
     #seller = list(set(org_ents)) #RECALL=0.69 (108/156); PRECISION=0.06 (108/1748); F-SCORE=0.11
     #TODO: use this high recall heuristic as a good starting point. Then use more heuristics (maybe SRL) to filter down
 
     acquired = n_most_common(4, org_ents)
+    #acquired = list(set(arg0_spans))
     #acquired = list(set(org_ents))
 
 
 
-
-
-
     #TODO: extract Aqcloc
-    # TODO: From buffer, generate pandas dataframe that effectively maps template fields to candidates and metadata about those candidates (using SRL)
+    #TODO: From buffer, generate pandas dataframe that effectively maps template fields to candidates and metadata about those candidates (using SRL)
     #TODO: implement step
     '''>Output SR labeled JSON to output files: one file per doc....>'''
     #TODO: implement
@@ -246,6 +335,7 @@ TOTAL           0.28 (469/1693)	   0.19 (469/2449)    0.23
                     acqloc=acqloc,
                     dlramt=dlramt,
                     purchaser=purchaser,
+                    acqbus=acqbus,
                     seller=seller)
 
 
@@ -257,7 +347,7 @@ def main():
     if sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print("USAGE: `python3 extract.py <docList> [-v]`")
         return
-    
+
     is_verbose = False
     if len(sys.argv) > 2:
         is_verbose = sys.argv[2] == '-v'
@@ -265,8 +355,8 @@ def main():
 
     '''Perform information extraction into Template objects'''
     if is_verbose : print('Loading models...')
-    #srl_predictor = SRLPredictor()
-    srl_predictor = None
+    srl_predictor = SRLPredictor()
+    #srl_predictor = None
     nlp = spacy.load("en_core_web_sm")
     if is_verbose : print('Models loaded.')
 
@@ -290,17 +380,20 @@ def main():
                                                     rule_based=True) # The rulebased model gaveme 0.01 higher F-score
     #if is_verbose : print('BATCHDATA_BATCH = %s\n' % batchdata_batch)
     if is_verbose : print('List of texts formatted.')
- 
+
     template_list = []
+    docnum = 1
     for doc_path, text_data in zip(doc_series, batchdata_batch):
+        if is_verbose : print('\n==============\nEXTRACTING FROM DOCUMENT #{}: {}\n==============\n'.format(docnum, doc_path))
         template_list.append(extract(doc_path,
                                      text_data,
                                      srl_predictor=srl_predictor,
                                      spacy_model=nlp,
                                      is_verbose=is_verbose))
+        docnum += 1
 
     '''Write to output file'''
-    output_file_path = OUTPUT_DIR_PATH + doclist_file_path.split('/')[-1] + '.templates' 
+    output_file_path = OUTPUT_DIR_PATH + doclist_file_path.split('/')[-1] + '.templates'
     with open(output_file_path, "w") as outf:
         for template in template_list:
             if is_verbose : print('%s\n' % template)
